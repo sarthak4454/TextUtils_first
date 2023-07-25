bcf3ea017e5f1c61dc1f34136046d6ac076dc67d
Sarthak Garg <garg49975@gmail.com>
2023-07-25 12:36:40 +0530

    first commit

--- dummy/src/components/NavBar.js
+++ dummy/src/components/NavBar.js
@@ -3,17 +3,17 @@ const NavBar=(props) =>{
         <div>
           <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">Navbarscroll</a>
+        <a className="navbar-brand" href="#">Navbarscroll</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarScroll">
           <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style={{"height": "80px"}}>
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">Home</a>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">Link</a>
+              <a className="nav-link" href="#">Link</a>
             </li>
             </ul>
           <form className="d-flex" role="search">
